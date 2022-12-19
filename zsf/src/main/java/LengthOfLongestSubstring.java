ecc8cba7011e0161325f79d232e96dd9087e3fc6
hjz <hujingzheng.hjz@alibaba-inc.com>
2022-12-19 17:38:51 +0800

    自定义缓存key的使用2

--- zsf/src/main/java/LengthOfLongestSubstring.java
+++ zsf/src/main/java/LengthOfLongestSubstring.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 public class LengthOfLongestSubstring {
 
     public static void main(String[] args) {
-        System.out.println(getNum("abccddddjkmnb"));
+        System.out.println(getNum("abccddddjkmnbbb"));
     }
 
     public static int getNum(String s){
