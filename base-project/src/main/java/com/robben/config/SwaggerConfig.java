67f5459faebf72452cc258f9fdfaa1a7bc2d2011
放山 <hujingzheng.hjz@alibaba-inc.com>
2023-08-29 17:00:21 +0800

    整理依赖关系

--- base-project/src/main/java/com/robben/config/SwaggerConfig.java
+++ base-project/src/main/java/com/robben/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.robben.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableKnife4j
 public class SwaggerConfig {
 
     @Bean
--- middleware/esUse/src/main/java/com/es/config/SwaggerConfig.java
+++ middleware/esUse/src/main/java/com/es/config/SwaggerConfig.java
@@ -1,41 +1,13 @@
 package com.es.config;
 
-import cn.hutool.core.util.RandomUtil;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
 public class SwaggerConfig {
-
-    /**
-     * 根据@Tag 上的排序，写入x-order
-     *
-     * @return the global open api customizer
-     */
-    @Bean
-    public GlobalOpenApiCustomizer orderGlobalOpenApiCustomizer() {
-        return openApi -> {
-            if (openApi.getTags()!=null){
-                openApi.getTags().forEach(tag -> {
-                    Map<String,Object> map=new HashMap<>();
-                    map.put("x-order", RandomUtil.randomInt(0,100));
-                    tag.setExtensions(map);
-                });
-            }
-            if(openApi.getPaths()!=null){
-                openApi.addExtension("x-test123","333");
-                openApi.getPaths().addExtension("x-abb",RandomUtil.randomInt(1,100));
-            }
-
-        };
-    }
 
 
     @Bean
