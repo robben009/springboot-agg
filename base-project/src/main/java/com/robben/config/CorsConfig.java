6024d3f4008aa6c9655a2691814b3598cb4e6c15
放山 <hujingzheng.hjz@alibaba-inc.com>
2023-10-16 20:47:12 +0800

    优化dubbo+nacos2

--- base-project/src/main/java/com/robben/config/CorsConfig.java
+++ base-project/src/main/java/com/robben/config/CorsConfig.java
@@ -1,35 +1,21 @@
 package com.robben.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Description： 解决跨域问题--使用Cors
- * Author: robben
- * Date: 2020/9/18 22:02
- *https://blog.csdn.net/jsiostream/article/details/85854891?utm_medium=distribute.pc_relevant.none-task-blog-title-5&spm=1001.2101.3001.4242
- */
+
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); // 4
-        return new CorsFilter(source);
-    }
-
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
-        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
-        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
-        corsConfiguration.setAllowCredentials(true);//支持安全证书。跨域携带cookie需要配置这个
-        corsConfiguration.setMaxAge(3600L);//预检请求的有效期，单位为秒。设置maxage，可以避免每次都发出预检请求
-        return corsConfiguration;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
     }
 
 }
