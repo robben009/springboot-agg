370b39e91543e31414cc7493bec7146270041eaf
放山 <hujingzheng.hjz@alibaba-inc.com>
2025-03-20 18:09:33 +0800

    增加堆排序

--- algorithm/src/main/java/sort/HeapSort.java
+++ algorithm/src/main/java/sort/HeapSort.java
@@ -1,31 +1,33 @@
 package sort;
 
+import java.util.Arrays;
+
 public class HeapSort {
     /* 堆的长度为 n ，从节点 i 开始，从顶至底堆化 */
-    void siftDown(int[] nums, int n, int i) {
+    static void siftDown(int[] nums, int n, int i) {
         while (true) {
-            // 判断节点 i, l, r 中值最大的节点，记为 ma
+            // 判断节点 i, l, r 中值最大的节点，记为 largest
             int l = 2 * i + 1;
             int r = 2 * i + 2;
-            int ma = i;
-            if (l < n && nums[l] > nums[ma])
-                ma = l;
-            if (r < n && nums[r] > nums[ma])
-                ma = r;
+            int largest = i;
+            if (l < n && nums[l] > nums[largest])
+                largest = l;
+            if (r < n && nums[r] > nums[largest])
+                largest = r;
             // 若节点 i 最大或索引 l, r 越界，则无须继续堆化，跳出
-            if (ma == i)
+            if (largest == i)
                 break;
             // 交换两节点
             int temp = nums[i];
-            nums[i] = nums[ma];
-            nums[ma] = temp;
+            nums[i] = nums[largest];
+            nums[largest] = temp;
             // 循环向下堆化
-            i = ma;
+            i = largest;
         }
     }
 
     /* 堆排序 */
-    void heapSort(int[] nums) {
+    static void heapSort(int[] nums) {
         // 建堆操作：堆化除叶节点以外的其他所有节点
         for (int i = nums.length / 2 - 1; i >= 0; i--) {
             siftDown(nums, nums.length, i);
@@ -39,5 +41,15 @@ public class HeapSort {
             // 以根节点为起点，从顶至底进行堆化
             siftDown(nums, i, 0);
         }
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 9, 7, 5, 11, 12, 2, 14, 3, 10, 6 };
+        System.out.println("原数组: " + Arrays.toString(array));
+
+        // 调用快速排序
+        heapSort(array);
+
+        System.out.println("排序后的数组: " + Arrays.toString(array));
     }
 }
