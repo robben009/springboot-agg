0bca6982de820f50e07c83e6798c2e62f74e1e32
放山 <hujingzheng.hjz@alibaba-inc.com>
2025-07-31 14:34:36 +0800

    增加netty-game示例2

--- nettyDemo/game/src/main/java/com/netty/game/server/NettyInitializer.java
+++ nettyDemo/game/src/main/java/com/netty/game/server/NettyInitializer.java
@@ -16,13 +16,21 @@ public class NettyInitializer extends ChannelInitializer<SocketChannel> {
     @Autowired
     private ApplicationContext context;
 
+
     @Override
     protected void initChannel(SocketChannel ch) {
-        ch.pipeline().addLast(new ProtobufVarint32FrameDecoder()); // 处理半包
-        ch.pipeline().addLast(new ProtobufDecoder(GameMsgDto.GameMsg.getDefaultInstance())); // 解码器
+        //基于 Protobuf varint32 变长编码的消息长度，实现拆分 TCP 粘包/半包现象。它根据 protobuf 的长度字段，将字节流切分成一条条完整的 protobuf 消息。
+        ch.pipeline().addLast(new ProtobufVarint32FrameDecoder());
+        // 解码器
+        ch.pipeline().addLast(new ProtobufDecoder(GameMsgDto.GameMsg.getDefaultInstance()));
 
+        /**
+         * 在发送端给每个消息前面加上 protobuf 的 varint32 格式的长度字段,这样，接收端可以知道每条消息的长度，正确拆包,
+         * 它是编码端（发送端）的长度字段写入器，和 ProtobufVarint32FrameDecoder 配合使用
+         */
         ch.pipeline().addLast(new ProtobufVarint32LengthFieldPrepender()); // 加长度
-        ch.pipeline().addLast(new ProtobufEncoder()); // 编码器
+        // 编码器
+        ch.pipeline().addLast(new ProtobufEncoder());
 
         ch.pipeline().addLast(context.getBean(NettyServiceHandler.class)); // 业务逻辑
     }
